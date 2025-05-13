3ead5bd19d41d4061f2c64f97f7d93892fa27eea
Peter Savinos <psavinos@depaul.edu>
2025-05-12 23:18:31 -0500

    Added FlyingBird interface - Extends Bird for birds that fly

---
+++ src/main/java/org/example/Solid/InterfaceSeggregation/Example2/Solution/FlyingBird.java
@@ -0,0 +1,8 @@
+package org.example.Solid.InterfaceSeggregation.Example2.Solution;
+
+/**
+ * Interface that extends Bird for flying birds
+ */
+public interface FlyingBird extends Bird {
+    void fly();
+}
